b4b301b0fa35b4a6e771ea242c6e36c90a4c5803
kimnewbie <gracecancho@gmail.com>
2023-02-02 00:07:16 +0900

    union type, any, unknown

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-var project = {
-    member: ["kim", "park"],
-    days: 30,
-    started: true,
+var 회원들 = [1, "2", 3];
+var 오브젝트 = { a: 123 };
+var 이름;
+이름 = 123;
+이름 = "김";
+이름 = {};
+// 이름 - 1; // --error
+var 나이;
+// 나이 + 1; // --error string + 1(허용) number + 1(허용) string | number + 1 (안돼)
+/**
+ * 숙제
+ */
+var user = "kim";
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: "Phil",
+    friend: "John",
 };
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
--- index.ts
+++ index.ts
@@ -1,9 +1,32 @@
-let project: {
-  member: string[];
-  days: number;
-  started: boolean;
+let 회원들: (number | string)[] = [1, "2", 3];
+let 오브젝트: { a: string | number } = { a: 123 };
+
+let 이름: unknown;
+이름 = 123;
+이름 = "김";
+이름 = {};
+
+// 이름 - 1; // --error
+
+let 나이: string | number;
+// 나이 + 1; // --error string + 1(허용) number + 1(허용) string | number + 1 (안돼)
+
+/**
+ * 숙제
+ */
+let user: string = "kim";
+let age: undefined | number = undefined;
+let married: boolean = false;
+let 철수: (string | number | undefined | boolean)[] = [user, age, married];
+
+let 학교: {
+  score: (number | boolean)[];
+  teacher: string;
+  friend: string[] | string;
 } = {
-  member: ["kim", "park"],
-  days: 30,
-  started: true,
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
 };
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
